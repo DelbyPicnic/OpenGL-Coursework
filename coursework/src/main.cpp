450838d36fedcec680a522263d2974f35e1dc9d8
Gordon Swan <40202556@live.napier.ac.uk>
2017-02-22 13:47:14 +0000

    Set updates and tidied up code - working on fixing lighting

--- coursework/src/main.cpp
+++ coursework/src/main.cpp
@@ -8,38 +8,53 @@ using namespace std;
 using namespace graphics_framework;
 using namespace glm;
 
-geometry geom;
+mesh groundPlane;
+mesh stdPyramid;
 effect eff;
 target_camera cam;
 
 bool load_content() {
-  // Surface plane data
-  vector<vec3> positions{
-	  vec3(-10.0f, 0.0f, -10.0f), vec3(-10.0f, 0.0f, 10.0f), vec3(10.0f, 0.0f, 10.0f),
-	  vec3(-10.0f, 0.0f, -10.0f), vec3(10.0f, 0.0f, 10.0f), vec3(10.0f, 0.0f, -10.0f)
-	  
+	// Surface plane data
+	geometry sPlane;
+	vector<vec3> sPlanePos{
+		vec3(-10.0f, 0.0f, -10.0f), vec3(-10.0f, 0.0f, 10.0f), vec3(10.0f, 0.0f, 10.0f),
+		vec3(-10.0f, 0.0f, -10.0f), vec3(10.0f, 0.0f, 10.0f), vec3(10.0f, 0.0f, -10.0f)
+	};
+  
+	// Add surface plane to the geometry
+	sPlane.add_buffer(sPlanePos, BUFFER_INDEXES::POSITION_BUFFER);
 
-  };
-  // Surface plane colours
-  vector<vec4> colours{
-	  vec4(1.0f, 0.0f, 0.0f, 1.0f), vec4(1.0f, 0.0f, 0.0f, 1.0f), vec4(1.0f, 0.0f, 0.0f, 1.0f),
-	  vec4(1.0f, 0.0f, 0.0f, 1.0f), vec4(1.0f, 0.0f, 0.0f, 1.0f), vec4(1.0f, 0.0f, 0.0f, 1.0f) 
-  };
-  // Add to the geometry
-  geom.add_buffer(positions, BUFFER_INDEXES::POSITION_BUFFER);
-  geom.add_buffer(colours, BUFFER_INDEXES::COLOUR_BUFFER);
+	// Add surface plane geometry to mesh
+	groundPlane = mesh(sPlane);
 
-  // Load in shaders
-  eff.add_shader("shaders/basic.vert", GL_VERTEX_SHADER);
-  eff.add_shader("shaders/basic.frag", GL_FRAGMENT_SHADER);
-  // Build effect
-  eff.build();
+	// Pyramid data
+	geometry sPyramid;
+	vector<vec3> sPyramidPos{
+		// Top (pointy bit)
+		vec3(0.0f, 2.0f, 0.0f), vec3(-1.0f, 0.0f, 1.0f), vec3(1.0f, 0.0f, 1.0f),
+		vec3(0.0f, 2.0f, 0.0f), vec3(1.0f, 0.0f, 1.0f), vec3(1.0f, 0.0f, -1.0f),
+		vec3(0.0f, 2.0f, 0.0f), vec3(1.0f, 0.0f, -1.0f), vec3(-1.0f, 0.0f, -1.0f),
+		vec3(0.0f, 2.0f, 0.0f), vec3(-1.0f, 0.0f, -1.0f), vec3(-1.0f, 0.0f, 1.0f)
+	};
+	
+	// Add pyramid to geometry
+	sPyramid.add_buffer(sPyramidPos, BUFFER_INDEXES::POSITION_BUFFER);
 
-  // Set camera properties
-  cam.set_position(vec3(0.0f, 5.0f, 10.0f));
-  cam.set_target(vec3(0.0f, 0.0f, 0.0f));
-  cam.set_projection(quarter_pi<float>(), renderer::get_screen_aspect(), 0.1f, 1000.0f);
-  return true;
+	// Add pyramid geometry
+	stdPyramid = mesh(sPyramid);
+
+	// Load in shaders
+	eff.add_shader("shaders/basic_colour.vert", GL_VERTEX_SHADER);
+	eff.add_shader("shaders/basic_colour.frag", GL_FRAGMENT_SHADER);
+
+	// Build effect
+	eff.build();
+
+	// Set camera properties
+	cam.set_position(vec3(0.0f, 5.0f, 10.0f));
+	cam.set_target(vec3(0.0f, 0.0f, 0.0f));
+	cam.set_projection(quarter_pi<float>(), renderer::get_screen_aspect(), 0.1f, 1000.0f);
+	return true;
 }
 
 
@@ -59,8 +74,14 @@ bool render() {
   auto MVP = P * V * M;
   // Set MVP matrix uniform
   glUniformMatrix4fv(eff.get_uniform_location("MVP"), 1, GL_FALSE, value_ptr(MVP));
+
+  // Set the colour value for the shader
+  glUniform4fv(eff.get_uniform_location("colour"), 1, value_ptr(vec4(0.765f, 0.082f, 0.196f, 1.0f)));
+
   // Render geometry
-  renderer::render(geom);
+  renderer::render(groundPlane);
+  glUniform4fv(eff.get_uniform_location("colour"), 1, value_ptr(vec4(0.5f, 0.3f, 1.0f, 1.0f)));
+  renderer::render(stdPyramid);
   return true;
 }
 
