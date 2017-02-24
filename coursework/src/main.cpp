899d240271d6759e9dceff6b0cdad4c1073e6b4c
Gordon Swan <40202556@live.napier.ac.uk>
2017-02-24 13:29:38 +0000

    Code updates and skybox addition

--- coursework/src/main.cpp
+++ coursework/src/main.cpp
@@ -10,7 +10,12 @@ using namespace glm;
 
 mesh groundPlane;
 mesh stdPyramid;
+mesh skybox;
 effect eff;
+effect sky_eff;
+effect light_eff;
+
+cubemap cube_map;
 
 free_camera cam;
 double cursor_x;
@@ -25,6 +30,28 @@ bool initialise() {
 }
 
 bool load_content() {
+	// ********************** LIGHTING LOAD **********************
+	light_eff.add_shader("shaders/env_light.vert", GL_VERTEX_SHADER);
+	light_eff.add_shader("shaders/env_light.frag", GL_FRAGMENT_SHADER);
+	light_eff.build();
+
+	// *********************** SKYBOX LOAD ***********************
+	// Create box geometry for skybox
+	skybox = mesh(geometry_builder::create_box(vec3(10.0f, 10.0f, 10.0f)));
+	// Scale box by 100
+	skybox.get_transform().scale = vec3(10.0f, 10.0f, 10.0f);
+	// Load the cubemap
+	array<string, 6> filenames = { "textures/sahara_ft.jpg", "textures/sahara_bk.jpg", "textures/sahara_up.jpg",
+		"textures/sahara_dn.jpg", "textures/sahara_rt.jpg", "textures/sahara_lf.jpg" };
+	// Create cube_map
+	cube_map = cubemap(filenames);
+	//Load skybox shaders
+	sky_eff.add_shader("shaders/skybox.vert", GL_VERTEX_SHADER);
+	sky_eff.add_shader("shaders/skybox.frag", GL_FRAGMENT_SHADER);
+	// Build Effect
+	sky_eff.build();
+
+	// *********************** OBJECTS LOAD **********************
 	// Surface plane data
 	geometry sPlane;
 	vector<vec3> sPlanePos{
@@ -57,10 +84,11 @@ bool load_content() {
 	// Load in shaders
 	eff.add_shader("shaders/basic_colour.vert", GL_VERTEX_SHADER);
 	eff.add_shader("shaders/basic_colour.frag", GL_FRAGMENT_SHADER);
-
+		
 	// Build effect
 	eff.build();
 
+	// *********************** CAMERA CONFIG **********************
 	// Set camera properties
 	cam.set_position(vec3(0.0f, 5.0f, 10.0f));
 	cam.set_target(vec3(0.0f, 0.0f, 0.0f));
@@ -117,34 +145,62 @@ bool update(float delta_time) {
 }
 
 bool render() {
-  // Bind effect
-  renderer::bind(eff);
-  // Create MVP matrix
-  mat4 M(1.0f);
-  auto V = cam.get_view();
-  auto P = cam.get_projection();
-  auto MVP = P * V * M;
-  // Set MVP matrix uniform
-  glUniformMatrix4fv(eff.get_uniform_location("MVP"), 1, GL_FALSE, value_ptr(MVP));
+	// *********************** SKYBOX RENDER ***********************
+	// Disable Depth Testing, Face Culling and Depth Masking
+	glDisable(GL_CULL_FACE);
+	glDisable(GL_DEPTH_TEST);
+	glDepthMask(GL_FALSE);
+	// Bind skybox effect
+	renderer::bind(sky_eff);
+	// Calculate MVP for Skybox
+	mat4 M = skybox.get_transform().get_transform_matrix();
+	auto V = cam.get_view();
+	auto P = cam.get_projection();
+	auto MVP = P * V * M;
+	// Set MVP Matrix Uniform
+	glUniformMatrix4fv(sky_eff.get_uniform_location("MVP"), 1, GL_FALSE, value_ptr(MVP));
+	//Set the CubeMap Uniform
+	renderer::bind(cube_map, 0);
+	glUniform1i(sky_eff.get_uniform_location("cubemap"), 0);
+	// Render Skybox
+	renderer::render(skybox);
+	// Enable Depth Testing, Face Culling and Depth Masking
+	glEnable(GL_CULL_FACE);
+	glEnable(GL_DEPTH_TEST);
+	glDepthMask(GL_TRUE);
 
-  // Set the colour value for the shader
-  glUniform4fv(eff.get_uniform_location("colour"), 1, value_ptr(vec4(0.765f, 0.082f, 0.196f, 1.0f)));
+	// ********************** OBJECTS RENDER ***********************
+	// Bind effect
+	renderer::bind(eff);
+	// Create MVP matrix
+	M = mat4(1.0f);
+	V = cam.get_view();
+	P = cam.get_projection();
+	MVP = P * V * M;
+	// Set MVP matrix uniform
+	glUniformMatrix4fv(eff.get_uniform_location("MVP"), 1, GL_FALSE, value_ptr(MVP));
 
-  // Render geometry
-  renderer::render(groundPlane);
-  glUniform4fv(eff.get_uniform_location("colour"), 1, value_ptr(vec4(0.5f, 0.3f, 1.0f, 1.0f)));
-  renderer::render(stdPyramid);
-  return true;
+	// Set the colour value for the shader
+	glUniform4fv(eff.get_uniform_location("colour"), 1, value_ptr(vec4(0.765f, 0.082f, 0.196f, 1.0f)));
+
+	// Render geometry
+	renderer::render(groundPlane);
+	glUniform4fv(eff.get_uniform_location("colour"), 1, value_ptr(vec4(0.5f, 0.3f, 1.0f, 1.0f)));
+	renderer::render(stdPyramid);
+
+	// ********************* LIGHTING CONFIG **********************
+	
+	return true;
 }
 
 void main() {
-  // Create application
-  app application("Graphics Coursework");
-  // Set load content, update and render methods
-  application.set_initialise(initialise);
-  application.set_load_content(load_content);
-  application.set_update(update);
-  application.set_render(render);
-  // Run application
-  application.run();
+	// Create application
+	app application("Graphics Coursework");
+	// Set load content, update and render methods
+	application.set_initialise(initialise);
+	application.set_load_content(load_content);
+	application.set_update(update);
+	application.set_render(render);
+	// Run application
+	application.run();
 }
